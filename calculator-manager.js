c089996ac19245f7f931e619a78dfa72f0772fc0
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-02-10 13:38:07 +0100

    build [core] add operate fn handles 2 numbers & operator and return the correct result

--- calculator-manager.js
+++ calculator-manager.js
@@ -1,7 +1,35 @@
+let numFirst;
+let numSecond;
+let operator;
+
+function operate(a, operator, b){
+    console.log(operator);
+ switch(operator){
+    case "+":
+        console.log(a,b);
+        return addNum(a, b);
+        break;
+
+    case "-":
+        return substractNum(a, b);
+        break;
+
+    case "*":
+        return multiplyNum(a, b);
+        break;
+
+    case "/":
+        return divideNum(a, b);
+        break;
+ }
+
+}
+
 //Basic math functions
 function addNum(a, b){
     return a + b;
 }
+//const addNum1 = (a, b) => a + b;
 
 function substractNum(a, b){
     return a - b;
