99fc10746e62801d68f7ba3d88576cbe0fc0de94
Katsoep <83074378+Katsoep@users.noreply.github.com>
2026-02-10 13:14:19 +0100

    build [core] add basic math functions

---
+++ calculator-manager.js
@@ -0,0 +1,16 @@
+//Basic math functions
+function addNum(a, b){
+    return a + b;
+}
+
+function substractNum(a, b){
+    return a - b;
+}
+
+function multiplyNum(a, b){
+    return a * b;
+}
+
+function divideNum(a, b){
+    return a / b;
+}
